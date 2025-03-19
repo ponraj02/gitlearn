0b3918c32a72a7faedc17e025baeeb1350bf28ba
ponraj <ponraj.s@cognizant.com>
2025-03-19 15:06:11 +0530

    initial commit

---
+++ src/main/java/com/example/gitlearnnew/GitlearnnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlearnnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlearnnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlearnnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitlearnnew/GitlearnnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlearnnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlearnnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
